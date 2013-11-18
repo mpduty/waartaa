a5a4554d6e0335ef8f060ca021f34e2300d2ef1e
rtnpro <rtnpro@gmail.com>
2013-11-19 02:05:58 +0530

    Using new node packages: dequeue, events.

--- waartaa/server/lib/01_requirements.js
+++ waartaa/server/lib/01_requirements.js
@@ -2,4 +2,6 @@ if (Meteor.isServer) {
 	irc = Npm.require('irc');
 	Fiber = Npm.require('fibers');
 	crypto = Npm.require('crypto');
+    Dequeue = Npm.require('dequeue');
+    events = Npm.require('events');
 }
